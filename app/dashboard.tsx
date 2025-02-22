e487dee0c7f6dc156bc3ac13bccb93a7f7f60766
Viraj Dey <viraj65.dey@gmail.com>
2025-02-22 23:57:27 +0530

    added onboarding and corrected route

---
+++ app/dashboard.tsx
@@ -0,0 +1,11 @@
+// app/dashboard.tsx
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function DashboardScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Dashboard Screen</Text>
+    </View>
+  );
+}
